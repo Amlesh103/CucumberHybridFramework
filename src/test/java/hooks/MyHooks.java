632322eb028bfa21711cb6193dee0581b2779bcb
Amlesh103 <96656985+Amlesh103@users.noreply.github.com>
2023-06-21 02:10:41 +0530

    Update MyHooks.java

--- src/test/java/hooks/MyHooks.java
+++ src/test/java/hooks/MyHooks.java
@@ -3,6 +3,7 @@ package hooks;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import factory.DriverFactory;
 import io.cucumber.java.After;
